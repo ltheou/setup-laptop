dc0686aa549c02e381c2e9250b053a33d2e1d24d
Charlie Root <root@bromn.home>
2018-02-18 23:27:22 +0100

    Missing files from the 1st run

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ pkg install beadm git ansible
 echo Setup laptop using [${PLAYBOOK}]
 
 if [ -e ${PLAYBOOK} ]; then
-  ansible-playbook -c local ${PLAYBOOK}
+  ansible-playbook ${PLAYBOOK} -i localhost, -c local
 else
   echo ERROR Ansible playbook [${PLAYBOOK}] not found !
   exit 12
